03e90fbf29ca5cba55abdb8fbc0cef466f92eb33
tgrassl <grassl.timon@gmail.com>
2025-03-03 00:40:42 +0100

    feat: wait until load before execute

--- packages/internal/src/index.ts
+++ packages/internal/src/index.ts
@@ -1,5 +1,5 @@
 export { loadCaptchaScript } from './loadCaptchaScript.js';
-export { withRetry } from './withRetry.js';
+export { withRetry, RetryError, TimeoutError } from './withRetry.js';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const isApiReady = () => typeof (window as any)?.captchafox !== 'undefined';
--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,3 +1,4 @@
+export { RetryError, TimeoutError } from '@captchafox/internal';
 export { CaptchaFox } from './CaptchaFox';
 export type { CaptchaFoxInstance } from './CaptchaFox';
 export const CAPTCHA_RESPONSE_KEY = 'cf-captcha-response';
